3de095692cf768042a36f5b79e42a5427a4d4e1d
ferdair <dairsanchez@gmail.com>
2022-07-10 00:58:22 -0500

    Agregado colores

--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -8,19 +8,26 @@
       </v-row>
 
       <v-divider></v-divider>
-
       <v-row class="mt-5">
         <v-col cols="12" v-for="vuelo in vuelosList" :key="vuelo.id">
           <v-card>
             <v-container>
               <v-row justify="space-between">
-                <v-col cols="4"> <p class="text-h5"> {{ vuelo.aerolinea }}</p> </v-col>
-                <v-col cols="4"> <p class="text-h4 blue--text">{{ vuelo.precio }} </p></v-col>
+                <v-col cols="4">
+                  <p class="text-h5">{{ vuelo.aerolinea }}</p>
+                </v-col>
+                <v-col cols="4">
+                  <p class="text-h4 blue--text">{{ vuelo.precio }}</p></v-col
+                >
               </v-row>
               <v-row justify="start">
-                <v-col cols="1"><v-icon color="blue">radio_button_checked </v-icon> </v-col
+                <v-col cols="1"
+                  ><v-icon color="blue">radio_button_checked </v-icon> </v-col
                 ><v-col cols="6" class="text-left">
-                   <span class="font-weight-bold blue--text">{{ vuelo.hora_salida }}</span> - {{ vuelo.ciudad_origen }}</v-col
+                  <span class="font-weight-bold blue--text">{{
+                    vuelo.hora_salida
+                  }}</span>
+                  - {{ vuelo.ciudad_origen }}</v-col
                 >
               </v-row>
               <v-row justify="start">
@@ -29,13 +36,16 @@
               <v-row justify="start">
                 <v-col cols="1"><v-icon>place </v-icon> </v-col>
                 <v-col cols="4" class="text-left"
-                  ><span class="font-weight-bold">{{ vuelo.hora_llegada }}</span> -
+                  ><span class="font-weight-bold">{{
+                    vuelo.hora_llegada
+                  }}</span>
+                  -
                   {{ vuelo.ciudad_destino }}
                 </v-col>
               </v-row>
               <v-row>
                 <v-col cols="4"
-                  ><v-icon  color="blue">flight </v-icon>
+                  ><v-icon color="blue">flight </v-icon>
                   {{ vuelo.n_escalas }} escala<template
                     v-if="vuelo.n_escalas > 0"
                     >s</template
